0a959f4b1b9f9e1e42e3dc31ec4b408b53ba8170
Adityas22 <adityasept2209@gmail.com>
2024-05-07 21:16:39 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// Data film
+const films = [
+    {
+        judul: "Inception",
+        tahun: 2010,
+        genre: "Science Fiction",
+        rating: 8.8
+    },
+    {
+        judul: "The Shawshank Redemption",
+        tahun: 1994,
+        genre: "Drama",
+        rating: 9.3
+    },
+    {
+        judul: "The Godfather",
+        tahun: 1972,
+        genre: "Crime",
+        rating: 9.2
+    }
+];
+
+// Endpoint untuk mengembalikan data film
+app.get('/', (req, res) => {
+    res.json(films);
+});
+
+// Port yang digunakan oleh server
+const PORT = process.env.PORT || 8080;
+
+// Mulai server
+app.listen(PORT, () => {
+    console.log(`Server berjalan pada port:${PORT}`);
+});
