3c4cc2a4c838ad27544471ba272fa1810a6d2665
Adityas22 <127333783+Adityas22@users.noreply.github.com>
2024-05-10 13:05:58 +0700

    update pada sevrer.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let biodata = {
   nim: "123210014",
   kelas: "IF-F",
   jurusan: "Informatika"
-  Fakultas : "Fakultas Teknik Industri"
+  Fakultas: "Fakultas Teknik Industri"
 };
 
 // Endpoint untuk mengembalikan biodata
