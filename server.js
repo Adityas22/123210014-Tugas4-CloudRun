0c8f325a80c1c659cec0f7679074f087106a8743
Adityas22 <adityasept2209@gmail.com>
2024-05-10 11:46:01 +0700

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
 
-// Data biodata
-const biodata = {
-    nama: "Aditya Septiawan",
-    nim: "123210014",
-    kelas: "IF-F",
-    jurusan: "Informatika"
+// Data biodata awal
+let biodata = {
+  nama: "Aditya Septiawan",
+  nim: "123210014",
+  kelas: "IF-F",
+  jurusan: "Informatika"
 };
 
 // Endpoint untuk mengembalikan biodata
 app.get('/', (req, res) => {
-    res.json(biodata);
+  res.json(biodata);
+});
+
+// Endpoint untuk memperbarui biodata
+app.post('/', (req, res) => {
+  // Mendapatkan data biodata yang diperbarui dari body permintaan
+  const updatedBiodata = req.body;
+
+  // Memperbarui biodata dengan data yang diterima
+  biodata = { ...biodata, ...updatedBiodata };
+
+  res.json(biodata);
 });
 
 // Port yang digunakan oleh server
@@ -19,6 +30,5 @@ const PORT = process.env.PORT || 8080;
 
 // Mulai server
 app.listen(PORT, () => {
-    console.log(`Server berjalan pada port :${PORT}`);
+  console.log(`Server berjalan pada port ${PORT}`);
 });
-
